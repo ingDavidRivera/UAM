4f1d6051fc0fb83b348b585d9596f538feb5a9b8
David Rivera A <davidriveraarbelaez@gmail.com>
2023-10-13 13:17:36 -0500

    Create TallerLoRa_Sender.cpp

---
+++ TallerLoRa_Sender.cpp
@@ -0,0 +1,35 @@
+/*******************************************
+* Taller LoRa - Semillero IoT              *
+* Emisor desde BSFrance LoRa32u4           *
+********************************************/
+#include <SPI.h>
+#include <LoRa.h>
+#define csPin 8
+#define resetPin 4 
+
+int contador = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+  LoRa.setPins(csPin, resetPin); 
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Comunicación fallida!");
+    while (1);
+  }
+}
+
+void loop() {
+  Serial.print("Enviando datos: ");
+  Serial.println(contador);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("Hola ");
+  LoRa.print(contador);
+  LoRa.endPacket();
+  contador++;
+  delay(5000);
+}
